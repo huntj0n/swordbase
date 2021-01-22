177006d069e318c089c3e6dbe20c80a152f27e06
huntj0n <hunt.jon19@gmail.com>
2021-01-22 16:38:12 -0700

    initial file structure

---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Projects.scss';
+
+const Projects = () => {
+    return (
+        <div className='projects'>
+            
+        </div>
+    )
+}
+
+export default Projects
