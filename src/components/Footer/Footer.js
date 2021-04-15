a879f030527345e2a5bff300ababac31939e2dfc
huntj0n <hunt.jon19@gmail.com>
2021-04-14 21:28:43 -0600

    revert from initializing new repo in wrong folder

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import './Footer.scss';
 
+//icons
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@material-ui/icons/GitHub';
+
 const Footer = () => {
     return (
         <div className='footer'>
-            Jon Hunt
+            <div className="footer__left">
+                Copyright 2021
+            </div>
+            <div className="footer__center">Jon Hunt</div>
+            <div className="footer__right">
+                <ul>
+                    <li><LinkedInIcon /></li>
+                    <li><GitHubIcon /></li>
+                </ul>
+            </div>
         </div>
     )
 }
